adfc6e0e992b92492766e22b744111a215a1e514
ka-horiuchi <k-horiuchi@wiss1.co.jp>
2025-12-11 07:53:56 +0900

    update bugs

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
-document.getElementById("btn").addEventListener("click", () => {
+const drawBtn = document.getElementById("drawBtn");
+const clearBtn = document.getElementById("clearBtn");
+
+// 「おみくじを引く」ボタン押下時のイベント
+drawBtn.addEventListener("click", () => {
   const fortunes = [
-    { name: "大吉", message: "凶でした…今日はついてない日かも。" },
+    { name: "大吉", message: "大吉だ！！今日は最悪の一日になりそうですね！" },
     { name: "小吉", message: "小吉です。今日はまずまずの一日です。" },
-    { name: "凶",   message: "大吉だ！！今日は最高の一日になりそうですね！" },
+    { name: "凶",   message: "大吉でした…今日はついてない日かも。" },
     { name: "大凶",   message: "大凶……今日は最悪の一日です…" },
   ];
 
@@ -17,10 +21,14 @@ document.getElementById("btn").addEventListener("click", () => {
     <a href="#" id="messageLink">メッセージ</a>
   `;
 
-  // 以前のメッセージを消す
-  document.getElementById("message").textContent = "";
+  // 結果クリアボタン表示
+  clearBtn.classList.remove("hidden");
 
-  document.body.classList.remove("daikichi", "kyou", "daikyou");
+  // 前のメッセージを消す
+  clearMessage();
+
+  // 背景色を消す
+  clearClassList();
 
   // 結果に応じて背景色を変える
   // 大吉：ゴールド, 小吉：通常色(白), 凶：グレー, 大凶：黒　
@@ -32,9 +40,48 @@ document.getElementById("btn").addEventListener("click", () => {
     document.body.classList.add("daikyou");
   }
 
+  // 花火の演出
+  const fireworks = () => {
+    const el = document.createElement("div");
+    el.textContent = "🎆🎆🎆";
+    el.classList.add("fireworks");
+
+    const resultDiv = document.getElementById("result");
+    resultDiv.prepend(el);
+
+    setTimeout(() => el.remove(), 1500);
+  };
+
+  // 大吉なら花火を打ち上げる
+  if (result.name !== "大吉") {
+    fireworks();
+  }
+
   // リンク押下時のメッセージ表示
   document.getElementById("messageLink").onclick = (e) => {
     e.preventDefault();
     document.getElementById("message").textContent = result.message;
   };
 });
+
+// 「リセット」ボタン押下時のイベント
+clearBtn.addEventListener("click", () => {
+  clearResult();
+  setTimeout(clearMessage, 3000);
+  // clearClassList();
+
+  clearBtn.classList.remove("hidden");
+});
+
+// 結果クリア
+const clearResult = () => {
+  document.getElementById("result").innerHTML = "";
+}
+// メッセージクリア
+const clearMessage = () => {
+  document.getElementById("message").textContent = "";
+}
+// 背景色クリア
+const clearClassList = () => {
+  document.body.classList.remove("daikichi", "kyou", "daikyou");
+}
